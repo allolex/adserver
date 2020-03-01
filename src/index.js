f70d102a6bb0ad303c79987707364660a8a19d72
Damon Davison <damon@curatur.com>
2020-03-01 14:06:04 -0500

    Protect write endpoints

--- src/index.js
+++ src/index.js
@@ -4,19 +4,41 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 const app = express();
 
 const {startDatabase} = require("./database/mongo");
 const {insertAd, getAds, deleteAd, updateAd} = require("./database/ads");
+
+
+const authZeroDomain = 'dev-n5g-rwr2.auth0.com';
 
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors())
 app.use(morgan("combined"));
 
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authZeroDomain}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://adserver',
+  issuer: `https://${authZeroDomain}/`,
+  algorithms: ['RS256']
+});
+
 app.get("/", async (req, res) => {
   res.send(await getAds());
 });
+
+app.use(checkJwt);
 
 app.post("/", async (req, res) => {
   const newAd = req.body;
